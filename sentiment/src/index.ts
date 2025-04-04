aa3d85bb5126c683cfbd8acca805137174944279
Mark Wong <markwong.digital@gmail.com>
2025-04-04 15:52:00 -0700

    Setup sentiment server with Express

---
+++ sentiment/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import pino from "pino-http";
+import logger from "./logger";
+import { globalErrorHandler } from "./error";
+
+/* CONFIGURATIONS */
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const app = express();
+app.use(express.json());
+app.use(pino({ logger }));
+
+// Global Error Handler
+app.use(globalErrorHandler);
+
+/* ROUTES */
+
+/* SERVER */
+const port = process.env.PORT || 5002;
+
+if (!isProduction) {
+	app.listen(port, () => {
+		logger.info(`Server running on port ${port}...`);
+	});
+}
