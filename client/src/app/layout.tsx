a4a3ac424e91f842e1aa5e4197a428861b7b13c5
Mark Wong <markwong.digital@gmail.com>
2025-04-04 21:33:57 -0700

    Setup frontend with Next.js

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserratSans = Montserrat({
+	variable: "--font-montserrat-sans",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "GiftLink | Give & Get Free Household Items Near You",
+	description:
+		"GiftLink connects generous givers with eco-conscious seekers. Browse, share, and claim free household items in your area. Sign up to reduce waste and find your next great gift—for free!",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${montserratSans.variable} antialiased`}>
+				<main className="">{children}</main>
+			</body>
+		</html>
+	);
+}
