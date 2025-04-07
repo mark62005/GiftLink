29036dae7b17953eff6ead4f3ad9aa12425241e2
Mark Wong <markwong.digital@gmail.com>
2025-04-07 01:29:37 -0700

    Fix css bugs

--- client/src/app/(main)/layout.tsx
+++ client/src/app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/shared/Header";
+import { HEADER_HEIGHT, MAIN_HEIGHT } from "@/lib/constants";
 import { ReactNode } from "react";
 
 export default function RootLayout({
@@ -9,7 +10,12 @@ export default function RootLayout({
 	return (
 		<>
 			<Header />
-			{children}
+			<main
+				className="bg-zinc-50"
+				style={{ marginTop: HEADER_HEIGHT, minHeight: MAIN_HEIGHT }}
+			>
+				{children}
+			</main>
 		</>
 	);
 }
