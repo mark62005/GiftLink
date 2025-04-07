b1fcd681a52d694e246deb393783e8337b64329f
Mark Wong <markwong.digital@gmail.com>
2025-04-07 01:40:27 -0700

    Build GiftCard component

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function getFormattedDate(timestamp: number): string {
+	const date = new Date(timestamp * 1000);
+	return date.toLocaleString("default", {
+		month: "long",
+		day: "numeric",
+		year: "numeric",
+	});
+}
--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -1,4 +1,19 @@
 declare global {
+	/* GIFTS */
+	interface Gift {
+		id: string;
+		name: string;
+		category: string;
+		condition: string;
+		posted_by: string;
+		zipcode: string;
+		date_added: number;
+		age_days: number;
+		age_years: number;
+		description: string;
+		image?: string;
+	}
+
 	/* UTILS */
 	interface NavLinkType {
 		label: string;
