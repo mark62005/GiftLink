57c28c5e24e6c4333d184e0615d1b4353907e299
Mark Wong <markwong.digital@gmail.com>
2025-04-09 21:17:49 -0700

    Add auth buttons on header

--- client/src/components/shared/Header/index.tsx
+++ client/src/components/shared/Header/index.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import Link from "next/link";
 import { HEADER_HEIGHT } from "@/lib/constants";
 import MaxWidthWrapper from "../MaxWidthWrapper";
 import Logo from "../Logo";
 import Navbar from "./Navbar";
+import { Button } from "@/components/ui/button";
+import NavLink from "./Navbar/NavLink";
+import { useAppSelector } from "@/state/redux";
 
 const NAV_LINKS_CONFIG: NavLinkType[] = [
 	{
@@ -15,6 +21,9 @@ const NAV_LINKS_CONFIG: NavLinkType[] = [
 ];
 
 function Header() {
+	const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
+	const user = useAppSelector((state) => state.auth.user);
+
 	return (
 		<header className="border-b-2 fixed top-0 left-0 right-0 z-2 bg-background">
 			<MaxWidthWrapper>
@@ -27,6 +36,21 @@ function Header() {
 					{/* RIGHT SIDE */}
 					<div className="flex justify-between items-center gap-2 md:gap-3">
 						<Navbar navLinks={NAV_LINKS_CONFIG} />
+
+						{!isLoggedIn ? (
+							<>
+								<Link href="/sign-in">
+									<Button variant="secondary">Sign In</Button>
+								</Link>
+
+								<NavLink navLink={{ href: "/sign-up", label: "Register" }} />
+							</>
+						) : (
+							<>
+								<span className="">Hi, {user?.firstName}!</span>
+								<Button variant="destructive">Sign Out</Button>
+							</>
+						)}
 
 						{/* TODO: Mobile nav menu */}
 					</div>
